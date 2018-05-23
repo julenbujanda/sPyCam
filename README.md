fdd93a3e43bb96022879b45be560faa3367cb265
julenb <21701719@live.uem.es>
2018-05-23 18:58:38 +0200

    Overlays

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # sPy Cam
+
+## Requisitos
+
+Se necesita un archivo de configuración con las API keys de Twitter llamado "autenticacion-py" en el directorio raíz.
+Debe tener la siguiente estructura:
+
+```
+consumer_key = 'CONSUMER_KEY'
+consumer_secret = 'CONSUMER_SECRET'
+access_token = 'ACCESS_TOKEN'
+access_token_secret = 'ACCESS_TOKEN_SECRET'
+```
+
+## Funcionamiento
+
--- sPyCam.py
+++ sPyCam.py
@@ -1,5 +1,6 @@
 from guizero import App, Text, Picture, PushButton
 from twython import Twython
+from picamera import PiCamera
 from autenticacion import (
     consumer_key,
     consumer_secret,
@@ -8,11 +9,15 @@ from autenticacion import (
 )
 
 twitter = Twython(consumer_key, consumer_secret, access_token, access_token_secret)
+camera = PiCamera()
+camera.resolution(800, 480)
+camera.hflip = True
+camera.start_preview(alpha=128)
+
 
 def tweetear():
-    #message = Text(app, "Text")
-    twitter.update_status(status='Nuevo Tweet 2')
-
+    # message = Text(app, "Text")
+    twitter.update_status(status='Iván cállate ya')
 
 
 app = App("The All Seeing Pi", 800, 480)
